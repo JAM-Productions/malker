65f274dc88fdffba1cf5f7d2daed2a4a351a1c35
Jordi <55429631+jorbush@users.noreply.github.com>
2023-11-28 21:38:39 +0100

    Colors palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pattens-blue': {
+          '50': '#f0faff',
+          '100': '#dcf1fd',
+          '200': '#bce7fb',
+          '300': '#80d6f9',
+          '400': '#3dc2f3',
+          '500': '#13abe4',
+          '600': '#0789c2',
+          '700': '#076d9d',
+          '800': '#0a5c82',
+          '900': '#0e4c6c',
+          '950': '#0a3147',
+        },      
+      },
+    },
   },
   plugins: [],
 }
