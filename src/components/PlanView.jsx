1f23d3e40568089c5bab6a22e9c338aa4ced0f0e
mriverre8 <rivemark00@gmail.com>
2023-12-08 18:54:12 +0100

    Update final
    Require final adjustments

--- src/components/PlanView.jsx
+++ src/components/PlanView.jsx
@@ -1,28 +1,60 @@
 import React from 'react';
 import { useState } from 'react';
+import Input from './form/Input';
+import Button from './form/Button';
+import { toast } from "react-toastify";
 
 const PlanView = () => {
     const [title, setTitle] = useState("Excursion Montserrat")
     const [author, setAuthor] = useState("Jordi Bonet")
     const [date, setDate] = useState("Dia")
     const [location, setLocation] = useState("Localizacion")
-    const [description, setDescription] = useState("Lorem ipsum dolor sit amet consectetur adipiscing elit, metus sem ridiculus aliquet primis natoque sociis netus, montes imperdiet placerat magnis orci quisque. Dui praesent fermentum orci id taciti, natoque placerat volutpat suscipit diam, tempus porta hac nulla. Inceptos nullam pellentesque volutpat egestas natoque consequat ultrices, cubilia a scelerisque felis parturient. Ac nisi volutpat quisque ultrices donec gravida sollicitudin felis tristique cras, id at ornare aliquet elementum ut conubia nec eget, condimentum penatibus commodo lobortis magnis maecenas a dignissim cum. Vulputate donec vitae ridiculus dictumst lectus posuere senectus, sagittis eu netus montes curabitur lacus euismod nibh, blandit semper a libero etiam vel. Lacinia himenaeos consequat nec aenean conubia aliquam odio fusce justo, egestas at rhoncus dui turpis potenti placerat integer magnis, sem vulputate etiam tincidunt pretium tellus luctus non.")
+    const [description, setDescription] = useState("Lorem ipsum dolor sit amet consectetur adipiscing elit, metus sem ridiculus aliquet primis natoque sociis netus, montes imperdiet placerat magnis orci quisque. Dui praesent fermentum orci id taciti, natoque placerat volutpat suscipit diam, tempus porta hac nulla. Inceptos nullam pellentesque volutpat egestas natoque consequat ultrices, cubilia a scelerisque felis parturient. Ac nisi volutpat quisque ultrices donec gravida sollicitudin felis tristique cras, id at ornare aliquet elementum ut conubia nec eget, condimentum penatibus commodo lobortis magnis maecenas a dignissim cum. Vulputate donec vitae ridiculus dictumst lectus posuere senectus, sagittis eu netus montes curabitur lacus euismod nibh, blandit semper a libero etiam vel. Lacinia himenaeos consequat nec aenean conubia aliquam odio fusce justo, egestas at rhoncus dui turpis potenti placerat integer magnis, sem vulputate etiam tincidunt pretium tellus luctus non. Lorem ipsum dolor sit amet consectetur adipiscing elit, metus sem ridiculus aliquet primis natoque sociis netus, montes imperdiet placerat magnis orci quisque. Dui praesent fermentum orci id taciti, natoque placerat volutpat suscipit diam, tempus porta hac nulla. Inceptos nullam pellentesque volutpat egestas natoque consequat ultrices, cubilia a scelerisque felis parturient. Ac nisi volutpat quisque ultrices donec gravida sollicitudin felis tristique cras, id at ornare aliquet elementum ut conubia nec eget, condimentum penatibus commodo lobortis magnis maecenas a dignissim cum. Vulputate donec vitae ridiculus dictumst lectus posuere senectus, sagittis eu netus montes curabitur lacus euismod nibh, blandit semper a libero etiam vel. Lacinia himenaeos consequat nec aenean conubia aliquam odio fusce justo, egestas at rhoncus dui turpis potenti placerat integer magnis, sem vulputate etiam tincidunt pretium tellus luctus non.")
 
+    const [name, setName] = useState("")
 
+    const resetForm = () => {
+        setName("")
+    }
+
+    const handleJoin = () => {
+        resetForm();
+        toast.info("Form cleared");
+    }
     return ( 
-        <div>
+        <section>
             {/* Información del plan */}
-            <div className="container px-5 py-24 mx-auto">
-                <div className="flex flex-col w-full">
+            <div className="container px-5 pt-20 pb-10 mx-auto">
+                <div className="flex flex-col w-10/12 mx-auto">
                     <h1 className="sm:text-3xl text-2xl font-medium text-gray-900">{title}</h1>
-                    <p className="mt-3 text-xs sm:text-base">{date} - {location}</p>
-                    <div className="border-t-2 border-gray-400 mx-3 mb-5 mt-3"></div>
-                    <p>{description}</p>
+                    <p className="mt-3 text-base ">{date} - {location}</p>
+                    <hr className="mt-3 mb-5 border-0 h-px bg-slate-400"></hr>
+                    <p className="overflow-auto max-h-72">{description}</p>
                     <p className="text-right mt-5">{author}</p>
                 </div>
             </div>
             {/* Unirse */}
-        </div>
+            <div className="container mx-auto">
+                <div className="flex flex-col items-center justify-center">
+                    <div className="p-2">
+                        <Input
+                            label={"Name"}
+                            type={"text"}
+                            value={name}
+                            onChange={setName}
+                            
+                            maxLength={30}
+                            />
+                    </div>
+                    <div className="p-2">
+                        <Button 
+                            text={"Join"}
+                            onClick={handleJoin}
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
