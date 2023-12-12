2c048565333bdb72e1b12642741622c9f05a8298
Jordi <55429631+jorbush@users.noreply.github.com>
2023-12-12 19:36:13 +0100

    Onrender

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import { execSync } from 'child_process'
 const currentBranch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig ({
   server: {
     proxy: {
-      '/api': {
+      baseUrl: {
         target: currentBranch !== 'main' ? 'http://127.0.0.1:5000' : 'https://malker-backend.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
@@ -17,7 +17,5 @@ export default defineConfig({
     host: true,
     strictPort: true,
   },
-  //base: currentBranch === 'main' ? '/malker' : '/',
-  mode: currentBranch === 'main' ? 'production' : 'development',
   plugins: [react()],
 })
