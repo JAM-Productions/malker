7024114e64feb5d89546f7fc90dbcb80b5dd5bc8
Jordi <55429631+jorbush@users.noreply.github.com>
2023-12-16 12:19:44 +0100

    Pylint in tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,10 +1,22 @@
+# pylint: disable = import-error
+# pylint: disable = redefined-outer-name
+"""
+Module containing fixtures for testing.
+"""
+
 import pytest
 from app import app as flask_app
 
 @pytest.fixture
 def app():
+    """
+    Fixture to provide the Flask app instance for testing.
+    """
     yield flask_app
-    
+
 @pytest.fixture
 def client(app):
+    """
+    Fixture to provide a test client for the Flask app.
+    """
     return app.test_client()
--- backend/tests/test_status.py
+++ backend/tests/test_status.py
@@ -1,6 +1,13 @@
+"""
+Module containing tests for the Flask app.
+"""
+
 import json
 
-def test_index(app, client):
+def test_index(client):
+    """
+    Test for the main route that checks if the response is as expected.
+    """
     res = client.get('/')
     assert res.status_code == 200
     expected = {'message': 'Hello from Malker!!'}
