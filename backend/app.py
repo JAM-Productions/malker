231d31cc921490da9993b37dd1563ba54c506550
AlbertMartinezM <amartimu65@alumnes.ub.edu>
2023-12-21 23:37:48 +0100

    disable csrf protection

--- backend/app.py
+++ backend/app.py
@@ -19,8 +19,7 @@ load_dotenv()
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET')
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
-app.config['JWT_CSRF_CHECK_FORM'] = False  # allows sending cookies through forms
-app.config['JWT_COOKIE_CSRF_PROTECT'] = False # just for testing
+app.config['JWT_COOKIE_CSRF_PROTECT'] = False
 app.config['JWT_SESSION_COOKIE'] = False  # makes cookies permanent
 jwt = JWTManager(app)
 api = Api(app)
