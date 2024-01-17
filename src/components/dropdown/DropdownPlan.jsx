0439c991c97c828353efd7c4e49388c83c12f3be
Jordi <55429631+jorbush@users.noreply.github.com>
2024-01-17 23:13:25 +0100

    Add animation description

--- src/components/dropdown/DropdownPlan.jsx
+++ src/components/dropdown/DropdownPlan.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { FaChevronDown } from 'react-icons/fa';
 
 const DropdownPlan = ({
@@ -9,6 +9,14 @@ const DropdownPlan = ({
   author
 }) => {
   const [open, setOpen] = useState(true);
+  const [descriptionHeight, setDescriptionHeight] = useState("auto");
+  const descriptionRef = useRef(null);
+
+  useEffect(() => {
+    if (descriptionRef.current) {
+      setDescriptionHeight(`${descriptionRef.current.scrollHeight + 10}px`);
+    }
+  }, [description]);
 
   const toggleDropdown = () => {
     setOpen(!open);
@@ -30,22 +38,30 @@ const DropdownPlan = ({
                     hover:text-blue-600
                     ${open ? 'rotate-180' : 'rotate-0'}
                     transition-transform
-                    duration-300
+                    duration-400
                     transform
-                `}/>
+                `}
+                />
               </button>
             </div>
           </div>
           <p className="mt-3 text-base">{date} - {location}</p>
         </div>
       </div>
-      {open && (
-        <div className="flex flex-col w-10/12 mx-auto">
+      <div
+        className="w-10/12 mx-auto"
+        style={{
+          maxHeight: open ? descriptionHeight : "0",
+          overflow: "hidden",
+          transition: "max-height 0.4s ease-in-out",
+        }}
+      >
+        <div ref={descriptionRef}>
           <hr className="mt-3 mb-5 border-0 h-px bg-slate-400"></hr>
           <p>{description}</p>
           <p className="text-right mt-5">{author}</p>
         </div>
-      )}
+      </div>
     </div>
   );
 };
