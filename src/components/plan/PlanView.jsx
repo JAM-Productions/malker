d72dc9c1299a0c72598649b500fe34aa5c8902c7
AlbertMartinezM <amartimu65@alumnes.ub.edu>
2024-02-05 21:42:44 +0100

    unify screens

--- src/components/plan/PlanView.jsx
+++ src/components/plan/PlanView.jsx
@@ -6,6 +6,7 @@ import { toast } from "react-toastify";
 import DropdownPlan from "../dropdown/DropdownPlan";
 import BackButton from "../navigation/BackButton";
 import {addParticipant, getPlanData, getUserData} from "../../comutils";
+import UserCard from "../user/UserCard";
 
 const PlanView = () => {
     const navigate = useNavigate()
@@ -18,6 +19,8 @@ const PlanView = () => {
     const [name, setName] = useState("")
     const [error, setError] = useState("")
     const [uuid, setUuid] = useState("")
+    const [participants, setParticipants] = useState();
+    const [joined, setJoined] = useState(false)
     const { id } = useParams();
 
     useEffect(() => {
@@ -27,6 +30,10 @@ const PlanView = () => {
             setDate(r.data.date)
             setLocation(r.data.location)
             setDescription(r.data.description)
+            setParticipants(r.data.participants)
+            console.log(r.data.participants)
+            console.log(participants)
+
         }).catch((e) => {
             toast.error('Plan not found')
             navigate('/malker/')
@@ -35,12 +42,20 @@ const PlanView = () => {
         // recover the username if the user already has one
         getUserData().then((r) => {
             setUuid(r.data.uuid)
+
+            // Check if the current user is in the list of participants
+            const currentUserExists = participants.some(participant => participant.uuid === uuid);
+            setJoined(currentUserExists);
+            console.log(participants)
+            console.log(currentUserExists)
+
             if (r.data.username !== null) {
                 setName(r.data.username)
             }
         }).catch((e) => {
             toast.error('You are not logged')
         })
+
     }, []);
 
     const handleJoin = () => {
@@ -53,7 +68,7 @@ const PlanView = () => {
         // call post endpoint
         addParticipant(id, uuid).then((r) => {
             toast.success("Join successfull");
-            navigate('/malker/show-participants')
+            setJoined(true)
         }).catch((e) => {
             toast.error("Can't join into the plan")
             console.log(e.toString())
@@ -65,7 +80,17 @@ const PlanView = () => {
             <BackButton />
             <DropdownPlan title={title} date={date} location={location} description={description} author={author}/>
 
-            {uuid !== '' && (
+            {joined && (
+                <div className="container mx-auto mb-10">
+                    <div
+                        className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-7 gap-10 w-10/12 mx-auto">
+                        {participants.map((user) => (
+                            <UserCard user={user.username} userUuid={user.uuid} currentUserUuid={uuid} key={user}/>
+                        ))}
+                    </div>
+                </div>
+            )}
+            {(uuid !== '' || !joined) && (
                 <div className="container mx-auto">
                     <div className="flex flex-col items-center justify-center">
                         <div className="p-2">
