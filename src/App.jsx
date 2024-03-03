c7598fa2fb9463474ef5bdaf776dab4e66523560
mriverre8 <rivemark00@gmail.com>
2024-03-03 19:51:20 +0100

    Delete PlanShowParticipants

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,44 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import PlanView from './components/plan/PlanView';
-import PlanForm from './components/plan/PlanForm';
-import PlanShowParticipants from './components/plan/PlanShowParticipants';
+import PlanView from "./components/plan/PlanView";
+import PlanForm from "./components/plan/PlanForm";
 
-import { getAuthToken } from './comutils'
+import { getAuthToken } from "./comutils";
 
 function App() {
+    useEffect(() => {
+        getAuthToken().then((token) => {
+            console.log(token);
+            //toast.success('account created')
+        });
+    }, []);
 
-  useEffect(() => {
-    getAuthToken().then((token) => {
-      console.log(token)
-      //toast.success('account created')
-    })
-  },[])
-
-  return (
-    <Router>
-      <div className='bg-malker-100 min-h-screen'>
-        <Header/>
-        <div className='pt-5 min-h-[88vh]'>
-          <ToastContainer toastStyle={{ backgroundColor: '#edf8fd' }}/>
-            <Routes>
-              <Route exact path="/malker" element={<PlanForm />} />
-              <Route path="/malker/plan-view/:id" element={<PlanView />} />
-              <Route path="/malker/show-participants" element={<PlanShowParticipants />} />
-            </Routes>
-        </div>
-        <Footer />
-      </div>
-    </Router>
-  )
+    return (
+        <Router>
+            <div className="bg-malker-100 min-h-screen">
+                <Header />
+                <div className="pt-5 min-h-[88vh]">
+                    <ToastContainer
+                        toastStyle={{ backgroundColor: "#edf8fd" }}
+                    />
+                    <Routes>
+                        <Route exact path="/malker" element={<PlanForm />} />
+                        <Route
+                            path="/malker/plan-view/:id"
+                            element={<PlanView />}
+                        />
+                    </Routes>
+                </div>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
-export default App
+export default App;
