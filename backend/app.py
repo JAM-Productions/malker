0faf7d11448bb4e65b83eff9720d4dbd6469a755
Jordi <55429631+jorbush@users.noreply.github.com>
2024-03-07 21:44:29 +0100

    Fix CORS (another time 😭)

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET')
 jwt = JWTManager(app)
 api = Api(app)
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+CORS(app, resources={r"*": {"origins": "*"}})
 
 api.add_resource(Login, '/api/login')
 api.add_resource(PlanAPI, '/api/plan', '/api/plan/<string:id>')
