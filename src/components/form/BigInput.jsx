c22f78fac795a46dc9583e2fc6615d45b49ef28c
mriverre8 <rivemark00@gmail.com>
2024-03-07 23:23:29 +0100

    npm run format

--- src/components/form/BigInput.jsx
+++ src/components/form/BigInput.jsx
@@ -1,12 +1,6 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-const BigInput = ({ 
-    label, 
-    onChange, 
-    value, 
-    error,
-    maxLength 
-}) => {
+const BigInput = ({ label, onChange, value, error, maxLength }) => {
     const [currentCharacters, setCurrentCharacters] = useState(0);
 
     const handleInputChange = (e) => {
@@ -16,21 +10,20 @@ const BigInput = ({
     };
 
     return (
-        <div className="relative">
+        <div className='relative'>
             <label
                 htmlFor={label}
-                className="
+                className='
                     leading-7 
                     text-sm 
                     text-gray-600
-                "
-            >
+                '>
                 {label.charAt(0).toUpperCase() + label.slice(1)}
             </label>
             <textarea
                 id={label}
                 title={label}
-                className="
+                className='
                     w-full 
                     bg-gray-100 
                     bg-opacity-50 
@@ -51,19 +44,18 @@ const BigInput = ({
                     transition-colors 
                     duration-200 
                     ease-in-out
-                "
+                '
                 value={value}
                 onChange={handleInputChange}
                 error={error}
-                maxLength={maxLength}
-            ></textarea>
+                maxLength={maxLength}></textarea>
             {currentCharacters > 0 && maxLength && (
-                <div className="absolute right-2 bottom-3 text-xs text-gray-500">
+                <div className='absolute right-2 bottom-3 text-xs text-gray-500'>
                     {currentCharacters} / {maxLength}
                 </div>
             )}
         </div>
     );
-}
+};
 
 export default BigInput;
