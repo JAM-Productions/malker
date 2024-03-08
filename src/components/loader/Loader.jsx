32b173161b3c60b95dede1e436bfcacac20236e2
mriverre8 <rivemark00@gmail.com>
2024-03-08 09:52:44 +0100

    Update Loader default params

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,14 +1,14 @@
 import { ProgressBar } from "react-loader-spinner";
 
-const Loader = () => {
+const Loader = ({ height = "47", width = "47", barColor = "#0789c2", borderColor = "#3dc2f3" }) => {
     return (
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <ProgressBar
                 visible={true}
-                height={"47"}
-                width={"47"}
-                barColor={"#0789c2"}
-                borderColor={"#3dc2f3"}
+                height={height}
+                width={width}
+                barColor={barColor}
+                borderColor={borderColor}
                 ariaLabel="progress-bar-loading"
             />
         </div>
