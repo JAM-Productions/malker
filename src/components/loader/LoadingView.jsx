f3f02c21e3ec8c1cfa494aaddcd5a3fa09a44d5f
jorbushv <jbonetv5@gmail.com>
2024-03-13 08:56:20 +0100

    Implement LoadingView

---
+++ src/components/loader/LoadingView.jsx
@@ -0,0 +1,15 @@
+import Loader from "./Loader";
+
+const LoadingView = ({ loading, children }) => { 
+    return (
+        <div>
+            {loading ? (
+                <Loader /> 
+            ) : ( 
+                children 
+            )}
+        </div>
+    );
+}   
+
+export default LoadingView;
--- src/components/plan/PlanForm.jsx
+++ src/components/plan/PlanForm.jsx
@@ -5,7 +5,7 @@ import Button from "../Button";
 import { toast } from "react-toastify";
 import BigInput from "../form/BigInput";
 import { createPlan } from "../../comutils";
-import Loader from "../loader/Loader";
+import LoadingView from "../loader/LoadingView";
 
 const PlanForm = () => {
     const navigate = useNavigate();
@@ -94,81 +94,78 @@ const PlanForm = () => {
     };
 
     return (
-        <div>
-            {loading && <Loader />}
-            {!loading && (
-                <section className="text-gray-600 body-font relative">
-                    <div className="container px-5 py-24 mx-auto">
-                        <div className="flex flex-col text-center w-full mb-12">
-                            <h1 className="sm:text-3xl  text-2xl font-medium title-font mb-4 text-gray-900 ">
-                                Create a new plan
-                            </h1>
-                            <p className="lg:w-2/3mx-autoleading-relaxedtext-base">
-                                Fill out the form below to create a new plan.
-                            </p>
-                        </div>
-                        <div className="lg:w-1/2 md:w-2/3 mx-auto ">
-                            <div className="flex flex-wrap -m-2 ">
-                                <div className="p-2 w-1/2">
-                                    <Input
-                                        label={"title"}
-                                        type={"text"}
-                                        value={title}
-                                        onChange={setTitle}
-                                        error={error}
-                                        maxLength={30}
-                                    />
-                                </div>
-                                <div className="p-2 w-1/2">
-                                    <Input
-                                        label={"author"}
-                                        type={"text"}
-                                        value={author}
-                                        onChange={setAuthor}
-                                        error={error && !author}
-                                        maxLength={20}
-                                    />
-                                </div>
-                                <div className="p-2 w-1/2">
-                                    <Input
-                                        label={"date"}
-                                        type={"date"}
-                                        value={date}
-                                        onChange={setDate}
-                                        error={error}
-                                    />
-                                </div>
-                                <div className="p-2 w-1/2">
-                                    <Input
-                                        label={"location"}
-                                        type={"text"}
-                                        value={location}
-                                        onChange={setLocation}
-                                        error={error}
-                                        maxLength={30}
-                                    />
-                                </div>
+        <LoadingView loading={loading}>
+            <section className="text-gray-600 body-font relative">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-col text-center w-full mb-12">
+                        <h1 className="sm:text-3xl  text-2xl font-medium title-font mb-4 text-gray-900 ">
+                            Create a new plan
+                        </h1>
+                        <p className="lg:w-2/3mx-autoleading-relaxedtext-base">
+                            Fill out the form below to create a new plan.
+                        </p>
+                    </div>
+                    <div className="lg:w-1/2 md:w-2/3 mx-auto ">
+                        <div className="flex flex-wrap -m-2 ">
+                            <div className="p-2 w-1/2">
+                                <Input
+                                    label={"title"}
+                                    type={"text"}
+                                    value={title}
+                                    onChange={setTitle}
+                                    error={error}
+                                    maxLength={30}
+                                />
+                            </div>
+                            <div className="p-2 w-1/2">
+                                <Input
+                                    label={"author"}
+                                    type={"text"}
+                                    value={author}
+                                    onChange={setAuthor}
+                                    error={error && !author}
+                                    maxLength={20}
+                                />
+                            </div>
+                            <div className="p-2 w-1/2">
+                                <Input
+                                    label={"date"}
+                                    type={"date"}
+                                    value={date}
+                                    onChange={setDate}
+                                    error={error}
+                                />
+                            </div>
+                            <div className="p-2 w-1/2">
+                                <Input
+                                    label={"location"}
+                                    type={"text"}
+                                    value={location}
+                                    onChange={setLocation}
+                                    error={error}
+                                    maxLength={30}
+                                />
+                            </div>
 
-                                <div className="p-2 w-full">
-                                    <BigInput
-                                        label={"description"}
-                                        onChange={setDescription}
-                                        value={description}
-                                        error={error && !description}
-                                        maxLength={250}
-                                    />
-                                </div>
+                            <div className="p-2 w-full">
+                                <BigInput
+                                    label={"description"}
+                                    onChange={setDescription}
+                                    value={description}
+                                    error={error && !description}
+                                    maxLength={250}
+                                />
+                            </div>
 
-                                <div className="p-2 w-full flex flex-row items-center">
-                                    <Button text={"Clear"} onClick={handleClear} />
-                                    <Button text={"Create"} onClick={handlePostPlan} />
-                                </div>
+                            <div className="p-2 w-full flex flex-row items-center">
+                                <Button text={"Clear"} onClick={handleClear} />
+                                <Button text={"Create"} onClick={handlePostPlan} />
                             </div>
                         </div>
                     </div>
-                </section>
-            )}
-        </div>
+                </div>
+            </section>
+        </LoadingView>
     );
 };
 
