a9fba1eaf717d1ffbfb80b4af4abb8aa43f77963
mriverre8 <rivemark00@gmail.com>
2024-03-24 14:40:30 +0100

    Refactor to only 1 spec
    Leave plan is not successfully tested because at joining it displays unknown user, and the test cant leave an unknown user.
    
    TODO

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,4 +1,6 @@
-describe("Init View", () => {
+let planId = "";
+
+describe("Init View PlanForm", () => {
     beforeEach(() => {
         cy.visit("http://localhost:5173/malker");
     });
@@ -26,7 +28,7 @@ describe("Init View", () => {
     });
 });
 
-describe("Functionality Tests", () => {
+describe("Functionality Tests PlanForm", () => {
     beforeEach(() => {
         cy.visit("http://localhost:5173/malker");
     });
@@ -77,7 +79,7 @@ describe("Functionality Tests", () => {
         cy.url().should("include", "/malker#/plan/");
 
         cy.url().then((url) => {
-            const planId = url.split("/").pop();
+            planId = url.split("/").pop();
             expect(planId).to.not.be.empty;
             expect(String(planId)).to.be.a("string");
         });
@@ -93,5 +95,91 @@ describe("Functionality Tests", () => {
         cy.get('button[id="Create"]').click();
 
         cy.url().should("eq", "http://localhost:5173/malker");
+    });
+});
+
+describe("Init View PlanView", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:5173/malker#/plan/" + planId);
+    });
+
+    it('Component "Header" check', () => {
+        cy.get('div[id="header"]').should("exist");
+    });
+
+    it('Component "Footer" check', () => {
+        cy.get('footer[id="footer"]').should("exist");
+    });
+
+    it("Screen components check", () => {
+        cy.get('div[id="back-button"]').should("exist");
+        cy.get('div[id="dropdown-plan"]').should("exist");
+
+        cy.get("h1[id=plan-title]").should("exist");
+        cy.get("h1#plan-title").should("not.have.text", "");
+
+        cy.get("button[id=dropdown-button]").should("exist");
+        cy.get("#dropdown-icon").should("have.class", "rotate-180");
+
+        cy.get("span[id=plan-date]").should("exist");
+        cy.get("span#plan-date").should("not.have.text", "");
+
+        cy.get("span[id=plan-location]").should("exist");
+        cy.get("span#plan-location").should("not.have.text", "");
+
+        cy.get("div[id=plan-actions]").should("exist");
+
+        cy.get("p[id=plan-description]").should("exist");
+        cy.get("p#plan-description").should("not.have.text", "");
+
+        cy.get('input[id="name"]').should("exist");
+        cy.get('button[id="Join"]').should("exist");
+
+        cy.get('input[id="name"]').type("Cypress Test");
+        cy.get('button[id="Join"]').click();
+
+        cy.get('div[id="user-card"]').should("exist");
+        cy.get('p[id="user-card-name"]').should("exist");
+        cy.get('p[id="user-card-tag"]').should("exist");
+
+        cy.get('button[id="Leave"]').should("exist");
+        cy.get('button[id="Leave"]').click();
+
+        //cy.get('input[id="name"]').should("exist");
+        //cy.get('button[id="Join"]').should("exist");
+    });
+});
+
+describe("Functionality Tests PlanView", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:5173/malker#/plan/" + planId);
+    });
+
+    it("Dropdown button click", () => {
+        cy.get("button[id=dropdown-button]").click();
+        cy.get("#dropdown-icon").should("have.class", "rotate-0");
+    });
+
+    it("Join plan without name", () => {
+        cy.get('input[id="name"]').clear();
+        cy.get('button[id="Join"]').click();
+        cy.get('input[id="name"]').should("exist");
+        cy.get('button[id="Join"]').should("exist");
+    });
+
+    it("Join plan and Leave plan", () => {
+        cy.get('input[id="name"]').type("Cypress Test");
+        cy.get('button[id="Join"]').click();
+
+        cy.get('div[id="user-card"]').should("exist");
+        cy.get('p[id="user-card-name"]').should("exist");
+        //cy.get("p#user-card-name").should("not.have.text", "unknown name");
+        cy.get('p[id="user-card-tag"]').should("exist");
+        //cy.get("p#user-card-tag").should("have.text", "You");
+        cy.get("button[id=Leave]").should("exist");
+
+        cy.get('button[id="Leave"]').click();
+        //cy.get('input[id="name"]').should("exist");
+        //cy.get('button[id="Join"]').should("exist");
     });
 });
